242bcff4362994b8e342eb33b40d6c25796f8bd0
Wolfgang Betz <wolfgang.betz@st.com>
2015-10-26 16:52:20 +0100

    Add macro for X-NUCLEO-IDB0XA1 expansion board

---
+++ CMake/toolchain.cmake
@@ -0,0 +1,14 @@
+# Copyright (C) 2014-2015 ARM Limited. All rights reserved.
+
+if(TARGET_ST_NUCLEO_F401RE_ST_BLE_GCC_TOOLCHAIN_INCLUDED)
+    return()
+endif()
+set(TARGET_ST_NUCLEO_F401RE_ST_BLE_GCC_TOOLCHAIN_INCLUDED 1)
+
+# provide compatibility definitions for compiling with this target: these are
+# definitions that legacy code assumes will be defined. Before adding something
+# here, think VERY CAREFULLY if you can't change anywhere that relies on the
+# definition that you're about to add to rely on the TARGET_LIKE_XXX
+# definitions that yotta provides based on the target.json file.
+#
+add_definitions("-DAST_FOR_MBED_OS")
